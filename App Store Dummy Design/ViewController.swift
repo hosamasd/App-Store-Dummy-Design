0c6a3bd73e3e182c0c7d81f8e03bd834dbc1b2ff
hosam mohamed <hosamelmalt@gmail.com>
2020-09-14 08:53:28 -0700

    Initial Commit

---
+++ App Store Dummy Design/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Store Dummy Design
+//
+//  Created by Hossam on 9/14/20.
+//  Copyright © 2020 Hossam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
